a831d9b09ca97d6b49495c66338f113032424c9b
Rik Huijzer <github@huijzer.xyz>
2025-04-27 15:35:55 +0200

    fix: set domain

--- backup.sh
+++ backup.sh
@@ -5,6 +5,6 @@ ARCHIVE_PATH="all.tar.xz"
 
 curl --proto "=https" --tlsv1.2 -sSf \
   -H "Authorization: Bearer $FX_PASSWORD" \
-  https://$DOMAIN/api/download/all.tar.xz > "$ARCHIVE_PATH"
+  https://fullyveganrecipes.com/api/download/all.tar.xz > "$ARCHIVE_PATH"
 
 tar --verbose -xf "$ARCHIVE_PATH"
